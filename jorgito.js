f6e1a301d8ae8217662b6c4b327f4c0d81c2825d
portuga4k <francisco.abreu195@gmail.com>
2024-04-17 12:34:09 +0100

    adição de switch cases
    adição de condições que mudem tanto a imagem como o texto dentro do botão com recurso a switch cases

--- jorgito.js
+++ jorgito.js
@@ -1,7 +1,29 @@
-function mudarImagem(){
-    //let imageOff =document.getElementById("imgLampadaOff");
-    console.log("adoro cock");
+function mudarElementos(){
+    //declaração de variáveis
+    let textoBotao = document.getElementById("botaoLigar").textContent;
+    let imageDisplay = document.getElementById("imgLampadaOff");
+    
+    
+    //intercalar entre os dois textos possíveis
+    switch(textoBotao){
+        case "Ligar":
+            textoBotao.textContent = "Desligar";
+        case "Desligar":
+            textoBotao.textContent ="Ligar";
+    }
+    
+    //mudar a imagem
+    switch (imageDisplay){
+        case "imgLampadaOff":
+            imageDisplay = "imgLampadaOn";
+        case "imgLampadaOn":
+            imageDisplay = "imgLampadaOff";
 
+    }
+
+    //console.log("adoro cock");
 }
 
-document.getElementById("botaoLigar").addEventListener("click", mudarImagem);
+//mudar o texto do botão quando é clicado
+document.getElementById("botaoLigar").addEventListener("click", mudarElementos);
+//document.getElementById("botaoLigar").addEventListener("click", mudarImagem);
